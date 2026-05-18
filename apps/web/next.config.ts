4f85c1f1a756bbe316e9347a4c61862bf8fe3686
Tiimie1 <tim.pangersic@gmail.com>
2026-05-18 15:15:06 +0200

    cicd: contabo deploy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "standalone",
+  outputFileTracingRoot: "../..",
   turbopack: {
     root: "../..",
   },
